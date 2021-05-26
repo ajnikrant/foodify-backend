46916e9f9fa90adfa7db55baefd1866f470222e1
Amanda <ajnikrant@gmail.com>
2021-05-26 14:19:07 -0400

    fixed serializer issue

--- app/serializers/cart_serializer.rb
+++ app/serializers/cart_serializer.rb
@@ -1,7 +1,5 @@
 class CartSerializer < ActiveModel::Serializer
   has_many :orders
-  # has_many :meals, through: :orders
-  # has_many :meals, through: :orders
   attributes :id, :checkedout, :user_id, :subtotal, :orders
   
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   has_many :carts
-  # has_many :orders
-  # has_many :meals
-
   attributes :id, :carts
 
 end
